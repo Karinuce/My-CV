f6d4a0647c2d204971c97209710fb286ab446975
Karinuce <karina.kavaleuskaya@gmail.com>
2023-02-28 17:04:47 +0200

    fixed media queries bugs

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
               <li>
                 <a
                   href="https://www.linkedin.com/in/karina-kavaleuskaya-a4a010214/"
-                  className="Soc-link "
+                  className="Soc-link"
                 >
                   <img
                     src={linkedin}
